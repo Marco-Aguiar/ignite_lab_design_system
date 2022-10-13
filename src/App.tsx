e73d2f6c24fa4377edd6b391c95311aee7268369
Marco-Aguiar <marcoaguiar14@gmail.com>
2022-10-13 09:33:58 -0300

    inicial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <div>
+      <h1 className='font-bold text-5xl text-violet-500'>acaceira chupa pica</h1>
+      <button className='font-bold text-gray-100'>CLICA AQUI CARAIO</button>
+    </div>
+    
+  )
+}
